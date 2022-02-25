4fb1b4f044a30947141631a534b14b13c94b66b4
CustomCodedIn2022 <101965@apps.everettsd.org>
2022-02-25 17:33:49 +0000

    c

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-button = Document.getElementById('button');
+button = document.getElementById('sub');
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function show(){
 
-    let div = Document.createElement('div');
-    div.innerHTML = '<h1>whyyyy whyyyyy you are so mean >:( </h1>'
-    Document.body.appendChild(div);
-    
-    }
-    
-    button.addEventListener('click', show, false);
+let div = document.createElement('div');
+div.innerHTML = '<p>haha you fell for it get ret nred</p>'
+document.body.appendChild(div);
+
+}
+
+button.addEventListener('click', show, false);
 
