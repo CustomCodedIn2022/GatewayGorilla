96f1f860fece562bd3485725881fed344294f7a5
CustomCodedIn2022 <101965@apps.everettsd.org>
2022-02-28 17:20:55 +0000

    c

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-button = document.getElementById('sub');
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+var pressed = document.querySelector("#sub");
 
-function show(){
-
-let div = document.createElement('div');
-div.innerHTML = '<p>haha you fell for it get ret nred</p>'
-document.body.appendChild(div);
-
-}
-
-button.addEventListener('click', show, false);
+var eventValue = function () {
+    document.body.appendChild(document.createElement('div')).textContent = 'how dare you :O';
+ }
+ 
+ pressed.addEventListener("click", eventValue);
 
