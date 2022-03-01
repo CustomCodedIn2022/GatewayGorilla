6c9bb3c286f61f8a9615ac89c86f1f93072fe6c0
CustomCodedIn2022 <101965@apps.everettsd.org>
2022-03-01 18:36:56 +0000

    E

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var pressed = document.html.body.querySelector("#sub");
-console.log(pressed);
-var EventClickButton = function () {
-    var message = document.html.body.appendChild(document.createElement('div')).innerHTML = '<iframe src="https://www.mathplayground.com/drift-boss-v2/index.html" height='800px' width='750px'></iframe> ';
+//var pressed = document.html.body.querySelector("#sub");
+//console.log(pressed);
+//var EventClickButton = function () {
+   //var message = document.html.body.appendChild(document.createElement('div')).innerHTML = '<iframe src="https://www.mathplayground.com/drift-boss-v2/index.html" height='800px' width='750px'></iframe> ';
     //console.log(message);
 
- }
+ //}
  
- pressed.addEventListener("click", EventClickButton, true);
+ //pressed.addEventListener("click", EventClickButton, true);
 
 
