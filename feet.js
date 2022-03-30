c9fd9930a7751d9234fe7377756384052aca3b11
CustomCodedIn2022 <101965@apps.everettsd.org>
2022-03-30 19:00:23 +0000

    com

---
+++ feet.js
@@ -0,0 +1,7 @@
+function fuctionfeet(){
+    for( var i = 0; i < 10; i++ ) {
+    window.open("https://www.google.com/search?q=dirty+fet+pictures&rlz=1CATAVM_enUS997&oq=dirty+fet+pictures&aqs=chrome..69i57.7101j0j7&sourceid=chrome&ie=UTF-8&safe=active&ssui=on", "_blank")
+    }
+}
+button = document.getElementById("dirtyfeetbutton");
+button.addEventListener("click", functionfeet);
